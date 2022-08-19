9ce40bdc4f45b7e422250582acd6b3a97aa7ac57
Nicholas Pettas <nick@faircrypto.org>
2022-08-18 20:07:40 -0700

    add some basic revert tests on rank functions

--- test/XENCrypto-Rank.test.js
+++ test/XENCrypto-Rank.test.js
@@ -9,14 +9,13 @@ const XENCrypto = artifacts.require("XENCrypto")
 
 const bn2hexStr = (bn) => '0x' + (bn?.toString(16)?.padStart(64, '0') || '0')
 
+
 contract("XEN Crypto (Rank amd XEN Claiming)", async accounts => {
 
     const genesisRank = 21
     let token
     let term = 2
-    let controlTs
     let expectedStakeId = genesisRank
-    let balance
 
     before(async () => {
         try {
@@ -73,6 +72,89 @@ contract("XEN Crypto (Rank amd XEN Claiming)", async accounts => {
 
     it("Should reject to withdraw stake before maturity", async () => {
         await assert.rejects(() => token.claimRankReward({from: accounts[1]}));
+    })
+
+    it("Should reject to claim rank reward when stake maturity not reached", async () => {
+        const snapshot = await timeMachine.takeSnapshot();
+        const snapshotId = snapshot['result'];
+
+        try {
+            await token.claimRank(5, {from: accounts[6]})
+            await truffleAssert.fails(
+                token.claimRankReward({from: accounts[6]}),
+                "CRank: Stake maturity not reached"
+            )
+        } catch (err) {
+            throw(err)
+        } finally {
+            await timeMachine.revertToSnapshot(snapshotId);
+        }
+    })
+
+    it("Should not reject claim rank reward", async () => {
+        const snapshot = await timeMachine.takeSnapshot();
+        const snapshotId = snapshot['result'];
+        const term = 5
+
+        try {
+            await token.claimRank(term, {from: accounts[6]})
+            await timeMachine.advanceTime(term * 24 * 3600 + 1)
+            await timeMachine.advanceBlock()
+            await token.claimRankReward({from: accounts[6]})
+        } catch (err) {
+            throw(err)
+        } finally {
+            await timeMachine.revertToSnapshot(snapshotId);
+        }
+    })
+
+    it("Should reject to claim rank reward and share when stake maturity not reached", async () => {
+        const snapshot = await timeMachine.takeSnapshot();
+        const snapshotId = snapshot['result'];
+
+        try {
+            await token.claimRank(5, {from: accounts[6]})
+            await truffleAssert.fails(
+                token.claimRankRewardAndShare(accounts[7], 50, {from: accounts[6]}),
+                "CRank: Stake maturity not reached"
+            )
+        } catch (err) {
+            throw(err)
+        } finally {
+            await timeMachine.revertToSnapshot(snapshotId);
+        }
+    })
+
+    it("Should reject to claim rank reward and share when no stake exists", async () => {
+        await truffleAssert.fails(
+            token.claimRankRewardAndShare(accounts[7], 50, {from: accounts[6]}),
+            "CRank: No stake exists"
+        )
+    })
+
+    it("Should reject to claim rank reward and share when sharing to zero address", async () => {
+        await truffleAssert.fails(
+            token.claimRankRewardAndShare('0x0000000000000000000000000000000000000000', 50, {from: accounts[6]}),
+            "CRank: Cannot share with zero address"
+        )
+    })
+
+    it("Should reject to claim rank reward and share when percent is less than 1", async () => {
+        await truffleAssert.fails(
+            token.claimRankRewardAndShare(accounts[7], 0, {from: accounts[6]}),
+            "CRank: Cannot share zero percent"
+        )
+
+        await truffleAssert.fails(
+            token.claimRankRewardAndShare(accounts[7], -1, {from: accounts[6]}),
+        )
+    })
+
+    it("Should reject to claim rank reward and share when percent is greater than 100", async () => {
+        await truffleAssert.fails(
+            token.claimRankRewardAndShare(accounts[7], 101, {from: accounts[6]}),
+            "CRank: Cannot share 100+ percent"
+        )
     })
 
     it("Should reject to claim rank with smaller than 1 day term", async () => {
@@ -132,4 +214,19 @@ contract("XEN Crypto (Rank amd XEN Claiming)", async accounts => {
         assert.ok(expectedRewardAmount === await token.totalSupply().then(_ => _.toNumber()))
     })
 
+    it("Should return user rank stake", async() => {
+        const term = Math.floor(Math.random() * (100 - 2) + 2)
+        const globalRank = await token.globalRank().then(_ => _.toNumber())
+        const blockNumber = await web3.eth.getBlockNumber();
+        const timestamp = (await web3.eth.getBlock(blockNumber)).timestamp
+        const maturityTs = timestamp + 3600 * 24 * term
+
+        await token.claimRank(term, {from: accounts[4]})
+        let rankState = await token.getUserRankStake({from: accounts[4]})
+
+        assert.equal(rankState.user, accounts[4])
+        assert.equal(rankState.term, term)
+        assert.equal(rankState.rank, globalRank)
+        assert.ok(rankState.maturityTs >= maturityTs)
+    })
 })
