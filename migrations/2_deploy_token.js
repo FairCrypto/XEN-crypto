d73aba4ed638cf75a4a8d97d72907fb529bb8bed
Leonid Belyaev <lbeliaev@gmail.com>
2022-10-18 11:41:02 -0700

    modified migration

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -3,6 +3,7 @@ const Math = artifacts.require("Math");
 
 module.exports = async function (deployer) {
   // await deployer.deploy(Math);
-  await deployer.link(Math, XENCrypto);
+  const math = Math.at('0x2AB0e9e4eE70FFf1fB9D67031E44F6410170d00e')
+  await deployer.link(math, XENCrypto);
   await deployer.deploy(XENCrypto);
 };
